0a1d5455ca11ae322cc1f0675192de409c77ed46
Darius Kasiulevičius <1218606+dariuskasiulevicius@users.noreply.github.com>
2020-12-27 21:47:26 +0200

    Day 9 solution

---
+++ src/Year2020/Day09/SilverPuzzle.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AdventOfCode\Year2020\Day09;
+
+use AdventOfCode\Year2020\DataInput;
+use AdventOfCode\Year2020\PuzzleResolver;
+
+class SilverPuzzle implements PuzzleResolver
+{
+    /**
+     * @return mixed
+     */
+    public function resolve(DataInput $inputData)
+    {
+        $result = 0;
+        $numbers = [];
+        foreach ($inputData as $item) {
+            //your custom code goes here
+            $numbers[] = (int) $item;
+        }
+        $result = $this->getFirstBad($numbers, 25);
+
+        return $result;
+    }
+
+    protected function getFirstBad($numbers, $size)
+    {
+        $stack = [];
+        $result = 0;
+        for ($i = 0; $i<$size; $i++) {
+            $stack[] = $numbers[$i];
+        }
+        $len = count($numbers);
+        for($k = $size; $k<$numbers; $k++) {
+            if (!$this->isValid($stack, $numbers[$k])){
+                $result = $numbers[$k];
+                break;
+            } else {
+                $stack[] = $numbers[$k];
+                array_shift($stack);
+            }
+        }
+
+        return $result;
+    }
+
+    protected function isValid($stack, $number)
+    {
+        foreach ($stack as $item1) {
+            foreach ($stack as $item2) {
+                if($item1+$item2 === $number){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
