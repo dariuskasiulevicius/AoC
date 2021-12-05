093e9783f5303fd6f0fad01d03673f1eaa9d8e7e
Darius Kasiulevičius <1218606+dariuskasiulevicius@users.noreply.github.com>
2021-12-05 09:38:27 +0200

    Added to print map

--- src/Year2021/Day05/SilverPuzzle.php
+++ src/Year2021/Day05/SilverPuzzle.php
@@ -14,6 +14,8 @@ class SilverPuzzle implements PuzzleResolver
     {
         $map = [];
         $actions = [];
+        $maxX = 0;
+        $maxY = 0;
         foreach ($inputData as $item) {
             if (false === preg_match('/(\d+),(\d+)[^\d]+(\d+),(\d+)/', $item, $matches)) {
                 echo 'error ' . $item . PHP_EOL;
@@ -22,7 +24,10 @@ class SilverPuzzle implements PuzzleResolver
             }
             array_shift($matches);
             $actions[] = array_map('intval', $matches);
+            $maxX = max($maxX, $matches[0], $matches[2]);
+            $maxY = max($maxY, $matches[1], $matches[3]);
         }
+        $map = array_fill(0, $maxY + 1, array_fill(0, $maxX + 1, 0));
 
         foreach ($actions as $action) {
             $x1 = $action[0];
@@ -58,11 +63,14 @@ class SilverPuzzle implements PuzzleResolver
 
         foreach ($map as $line) {
             foreach ($line as $item) {
+                echo $item === 0 ? '.' : $item;
                 if ($item >= 2) {
                     $result++;
                 }
             }
+            echo PHP_EOL;
         }
+        echo PHP_EOL;
 
         return $result;
     }
