805e360eb988988d07288472b790e7a8aee1553f
Darius Kasiulevičius <1218606+dariuskasiulevicius@users.noreply.github.com>
2024-12-07 14:26:01 +0200

    2024 Day 07 solution

---
+++ src/Year2024/Day07/GoldPuzzle.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AdventOfCode\Year2024\Day07;
+
+use AdventOfCode\Year2024\DataInput;
+use AdventOfCode\Year2024\PuzzleResolver;
+
+class GoldPuzzle implements PuzzleResolver
+{
+    /**
+     * @return mixed
+     */
+    public function resolve(DataInput $inputData)
+    {
+        $result = 0;
+
+        foreach ($inputData as $item) {
+            [$testSum, $elements] = explode(': ', $item);
+            $numbers = array_map('intval', explode(' ', $elements));
+            $count = count($numbers);
+            $sums = [$numbers[0]];
+            for ($i = 1; $i < $count; $i++) {
+                $newSums = [];
+                foreach ($sums as $sum) {
+                    if ($sum + $numbers[$i] <= $testSum) {
+                        $newSums[] = $sum + $numbers[$i];
+                    }
+                    if ($sum * $numbers[$i] <= $testSum) {
+                        $newSums[] = $sum * $numbers[$i];
+                    }
+                    $length = strlen((string)$numbers[$i]);
+                    if ($sum * (10 ** $length) + $numbers[$i] <= $testSum) {
+                        $newSums[] = $sum * (10 ** $length) + $numbers[$i];
+                    }
+                }
+                $sums = $newSums;
+            }
+            foreach ($sums as $sum) {
+                if ($sum == $testSum) {
+                    $result += $sum;
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
